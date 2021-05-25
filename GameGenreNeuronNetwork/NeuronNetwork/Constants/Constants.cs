6f3a37d6b6a4ed825e83a54bd2d8cac6287e2512
Denys Skibinskyi <ruslanskibinskii@gmail.com>
2021-05-25 04:41:44 +0300

    Ended CourseWork

--- GameGenreNeuronNetwork/NeuronNetwork/Constants/Constants.cs
+++ GameGenreNeuronNetwork/NeuronNetwork/Constants/Constants.cs
@@ -1,12 +1,9 @@
-﻿using GameGenreNeuronNetwork.Context;
-using System.Linq;
-
-namespace GameGenreNeuronNetwork.NeuronNetwork
+﻿namespace GameGenreNeuronNetwork.NeuronNetwork
 {
     public class Constants
     {
         public const int NumberOfHiddenNeurons = 10;
-        public const double Accuracy = 0.1;
+        public const double Accuracy = 0.01;
         public const double LearningSpeed = 0.1;
     }
 }
